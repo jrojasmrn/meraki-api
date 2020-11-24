7bc736da77a71f6970b9b59c1125efe459439ded
rubikazzbassmrn <rubikazzbassmrn@gmail.com>
2020-11-23 22:23:42 -0600

    1023

--- conectivity_info.py
+++ conectivity_info.py
@@ -19,55 +19,43 @@ def conectivity_info(vehicle_id, device_id):
     # Este es el ID de la red en Meraki
     network_id = 'N_584342051651361715'
 
-    # Obtenemos  el historial de datos de una unidad
-    response_data = dashboard.sm.getNetworkSmDeviceCellularUsageHistory(
-        network_id,
-        device_id
-    )
-    # Obtenemos el historial de conectividad del telefono
-    response_con = dashboard.sm.getNetworkSmDeviceConnectivity(
-        network_id,
-        device_id,
-        total_pages='all',
-        direction='next'
-    )
-    # Obtenemos la fecha del registro
-    received_on = {
-        'Received_on': datetime.now()
-    }
-
+    # Obtenemos los datos, bateria, última conexión y posición
     response = dashboard.sm.getNetworkSmDevices(
         network_id,
         total_pages='all',
-        fields=['batteryEstCharge', 'lastConnected']
+        fields=['batteryEstCharge', 'lastConnected', 'cellularDataUsed']
     )
-    print(response)
+    # Recorremos el diccionario para borrar unos indices que no nos sirven
+    for i in range(len(response)):
+        response[i].pop('name')
+        response[i].pop('tags')
+        response[i].pop('ssid')
+        response[i].pop('wifiMac')
+        response[i].pop('osName')
+        response[i].pop('systemModel')
+        response[i].pop('uuid')
+        response[i].pop('serialNumber')
+        response[i].pop('hasChromeMdm')
 
     # Comienza la insercion a la base de datos
-    # try:
-    #     with conexion.cursor() as cursor:
-    #         # Sustituimos los None por null en el dict
-    #         for key in response_data:
-    #             if key['received'] == None:
-    #                 key.update(received='null')
-    #             if key['sent'] == None:
-    #                 key.update(sent='null')
-    #         # Obtenemos el último registro de response_con
-    #         for key2 in response_con:
-    #             pass
-    #         # Combinamos ambos Dicts
-    #         key.update(key2)
-    #         # Ingresamos la fecha de creación del registro
-    #         key['Received_On'] = datetime.now()
-    #         values = ', '.join("'" + str(x).replace('/', '_') + "'" for x in key.values())
-    #         sql = "INSERT INTO %s (Vehicle_Id, Meraki_id, Received, Sent, Ts, First_Seen_At, Last_Seen_At, Received_On) VALUES (%s, %s, %s);" % ('Meraki.conectivity_info_device', vehicle_id, device_id, values)
-    #         cursor.execute(sql)
-    #         cursor.commit()
-    #         print("Se inserto con exito")
-    #         # print(sql)
-    # except Exception as e:
-    #     print("Ocurrio un error al insertar: ", e)
-    # finally:
-    #     conexion.close()
+    try:
+        with conexion.cursor() as cursor:
+            # Sustituimos los None por null en el dict
+            for key in response:
+                if key['cellularDataUsed'] == None:
+                    key.update(cellularDataUsed='null')
+                # Ingresamos la fecha de creación del registro
+                key['Received_On'] = datetime.now()
+                # Visualizamos el dict completo
+                # print(key)
+                values = ', '.join("'" + str(x).replace('/', '_') + "'" for x in key.values())
+                sql = "INSERT INTO %s (Vehicle_Id, Meraki_id, Battery_Level, Last_Connected, Data_Used, Location, Received_On) VALUES (%s, %s);" % ('conectivity_info_device', vehicle_id, values)
+                # cursor.execute(sql)
+                # cursor.commit()
+                print(sql)
+    except Exception as e:
+        print("Ocurrio un error al insertar: ", e)
+    finally:
+        conexion.close()
 
 conectivity_info(316, 584342051651873968)
